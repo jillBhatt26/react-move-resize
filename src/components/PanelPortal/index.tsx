af078b708c06bbf2fa60877e20dd8df48ccb1644
jill bhatt <bhattjill@gmail.com>
2023-02-19 00:47:45 +0530

    Removed unused dependencies

--- src/components/PanelPortal/index.tsx
+++ src/components/PanelPortal/index.tsx
@@ -1,19 +1,13 @@
-import usePortal from 'react-useportal';
 import Panel from '../Panel';
 import './styles.css';
 
 const PanelPortal = (): JSX.Element => {
-    // hooks
-    const { Portal } = usePortal({
-        bindTo: document.getElementById('panel-portal') as HTMLDivElement
-    });
-
     return (
-        <Portal>
+        <div>
             <Panel>
                 <div className="panel_content">Content</div>
             </Panel>
-        </Portal>
+        </div>
     );
 };
 
